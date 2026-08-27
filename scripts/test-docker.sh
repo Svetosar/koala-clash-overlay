a86d743d78ccd016fcf0a2b2316509625d87ece9
Svetosar <aparfenetv730@gmail.com>
2026-08-27 17:53:12 +0300

    Remove Docker test

--- scripts/test-docker.sh
+++ scripts/test-docker.sh
@@ -1,10 +1,4 @@
 #!/bin/sh
 set -e
 
-echo "=== Building Docker test image ==="
-docker build -f scripts/docker/Dockerfile.test -t koala-clash-test .
-
-echo "=== Running emerge dry-run ==="
-docker run --rm koala-clash-test
-
-echo "=== DOCKER TEST PASSED ==="
+echo "=== Docker test removed ==="
